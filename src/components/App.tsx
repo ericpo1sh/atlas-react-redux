c5893e5c96f4c10894e96fc168927ca518c87d5f
ericpo1sh <ericpo1sh@gmail.com>
2024-09-24 17:30:05 -0500

    5. Local Storage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,18 @@
 import Header from "./Header";
 import Footer from "./Footer"
 import Board from './Board'
-import { store } from "../store";
+import { persistor, store } from "../store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <Provider store={store}>
-      <Header/>
-      <Board/>
-      <Footer/>
+      <PersistGate loading={null} persistor={persistor}>
+        <Header/>
+        <Board/>
+        <Footer/>
+      </PersistGate>
     </Provider>
   )
 }
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from "react-redux";
 import listsReducer from './slices/listsSlice'
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
+
+const persistedListsRecuder = persistReducer(
+  { key: "lists", storage },
+  listsReducer 
+);
 
 export const store = configureStore({
   reducer: {
-    lists: listsReducer, 
+    lists: persistedListsRecuder, 
   },
 });
 
@@ -15,3 +22,5 @@ export type AppDispatch = typeof store.dispatch;
 //Add types to dispatch and selector hooks
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
+
+export const persistor = persistStore(store);
